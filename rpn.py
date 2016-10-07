f44bc0a9e0c06de0c821de7f8ab07742ee3ac793
Thomas Huang <thomaseh@umich.edu>
2016-10-07 11:54:14 -0700

    Refactor for common operators code

--- rpn.py
+++ rpn.py
@@ -1,31 +1,26 @@
 #!/usr/bin/env python3
 
+import operator
+
+operators = {
+	'+': operator.add,
+	'-': operator.sub,
+	'*': operator.mul,
+	'/': operator.truediv,
+}
+
 def calculate(myarg1):
 	stack = list();
 	for token in myarg1.split():
-			if token == '+':
-				arg1 = stack.pop()
-				arg2 = stack.pop()
-				result = arg1 + arg2
-				stack.append(result)
-			elif token == '-':
-				arg2 = stack.pop()
-				arg1 = stack.pop()
-				result = arg1 - arg2
-				stack.append(result)
-			elif token == '*':
-				arg2 = stack.pop()
-				arg1 = stack.pop()
-				result = arg1 * arg2
-				stack.append(result)
-			elif token == '/':
-				arg2 = stack.pop()
-				arg1 = stack.pop()
-				result = arg1 / arg2
-				stack.append(result)
-			else:
-				stack.append(int(token))
-			print(stack)
+		try:
+			stack.append(int(token))
+		except ValueError:
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			function = operators[token]
+			result = function(arg1, arg2)
+			stack.append(result)
+		print(stack)
 	if len(stack) != 1:
 		raise TypeError
 	return stack.pop()
