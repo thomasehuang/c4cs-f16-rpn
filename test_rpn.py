9c69d2041fae8e21e00c4030e200ce71e5e9e2de
Thomas Huang <thomaseh@umich.edu>
2016-10-07 11:42:31 -0700

    multiply and divide tests and implementation

--- test_rpn.py
+++ test_rpn.py
@@ -12,3 +12,10 @@ class TestBasics(unittest.TestCase):
 	def test_toomanythings(self):
 		with self.assertRaises(TypeError):
 			rpn.calculate("1 2 3 +");
+	def test_multiply(self):
+		result = rpn.calculate("2 4 *")
+		self.assertEqual(8, result)
+
+	def test_divide(self):
+		result = rpn.calculate("8 2 /")
+		self.assertEqual(4, result)	
